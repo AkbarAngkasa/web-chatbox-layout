276d552f047b8a20f1a4f057a58c1d5395298ac6
unknown <akbar@emkop.co.id>
2023-03-20 18:51:38 +0800

    modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-// import MobileCamera from "./MobileCamera"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import Home from './Home';
+import MobileCamera from "./MobileCamera"
 import PinLayout from "./PinLayout"
 
 export default function App() {
   return(
-    <div>
-      <PinLayout />
-    </div>
+    <Router>
+      <div className="content select-none">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/camera-layout" element={<MobileCamera />}/>
+          <Route path="/pin-screen-layout" element={<PinLayout />}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold underline flex flex-col">
+        <Link to={'/camera-layout'}>Camera Layout</Link>
+        <Link to={'/pin-screen-layout'}>PIN Screen Layout</Link>
+    </div>
+  )
+}
