0377b7875a177f7e9bac4586c4d555f79475376a
unknown <akbar@emkop.co.id>
2023-05-03 12:26:52 +0800

    modified NavbarBlockFirst.jsx

--- src/components/blocks/NavbarBlockFirst.jsx
+++ src/components/blocks/NavbarBlockFirst.jsx
@@ -17,10 +17,14 @@ export default function NavbarBlockFirst() {
                 {/* Wrapper */}
                 <div className="flex flex-wrap justify-start">
                     {/* Sidenav Open Btn Mobile */}
-                    <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="mr-4 text-xl rounded-lg text-gray-500 dark:text-gray-400 dark:focus:ring-gray-600 sm:hidden">
-                        <span className="sr-only">Open sidebar</span>
+                    <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="mr-4 text-xl text-gray-500 dark:text-gray-400 dark:focus:ring-gray-600 sm:hidden">
                         <FontAwesomeIcon icon={faBars} />
                     </button>
+
+                    <div className='flex flex-col'>
+                        <span className="text-sm font-medium mb-2 text-slate-600 dark:text-white hidden sm:inline-block">Backoffice</span>
+                        <span className="text-2xl font-semibold text-slate-600 dark:text-white inline-block">Pending KYC</span>
+                    </div>
                 </div>
 
                 {/* Wrapper */}
@@ -154,8 +158,7 @@ export default function NavbarBlockFirst() {
                     {/* End of Apps Button */}
 
                     {/* User Button  */}
-                    <button type="button" className="w-8 h-8 ml-4 text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
-                        <span className="sr-only">Open user menu</span>
+                    <button type="button" data-dropdown-toggle="dropdown" aria-expanded="false" id="user-menu-button" className="ml-4 text-xl rounded-lg text-gray-500 dark:text-gray-400 dark:focus:ring-gray-600">
                         <img className="w-8 h-8 rounded-full" src={user1} alt={user1} />
                     </button>
                     <div className="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown">
@@ -173,19 +176,17 @@ export default function NavbarBlockFirst() {
                         </ul>
                         <ul className="py-1 font-light text-gray-500 dark:text-gray-400" aria-labelledby="dropdown">
                             <li>
-                                <button href="#" className="flex items-center py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><svg className="mr-2 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd"></path></svg> My likes</button>
-                            </li>
-                            <li>
                                 <button href="#" className="flex items-center py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><svg className="mr-2 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg> Collections</button>
                             </li>
-                            <li>
+                            
+                            {/* <li>
                                 <button href="#" className="flex justify-between items-center py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                     <span className="flex items-center">
                                         <svg aria-hidden="true" className="mr-2 w-5 h-5 text-primary-600 dark:text-primary-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clipRule="evenodd"></path></svg> Pro version
                                     </span>
                                     <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                 </button>
-                            </li>
+                            </li> */}
 
                         </ul>
                         <ul className="py-1 font-light text-gray-500 dark:text-gray-400" aria-labelledby="dropdown">
